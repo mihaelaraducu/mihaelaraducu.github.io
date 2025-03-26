9778f0a9415b7641216018823984214fabd46d88
mihaela <mihaela@demo.ro>
2025-03-26 20:54:55 +0200

    mycv

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,7 @@ const myHeader = fs.readFileSync(paths.source + '/views/header.html');
 const myBanner = fs.readFileSync(paths.source + '/views/banner.html');
 const myAbout = fs.readFileSync(paths.source + '/views/about.html');
 const myExp = fs.readFileSync(paths.source + '/views/experience.html');
+const myPort = fs.readFileSync(paths.source + '/views/portfolio.html');
 const myContact = fs.readFileSync(paths.source + '/views/contact.html');
 const myFooter = fs.readFileSync(paths.source + '/views/footer.html');
 module.exports = {
@@ -39,6 +40,7 @@ module.exports = {
             myBanner: myBanner,
             myAbout: myAbout,
             myExperience: myExp,
+            myPortfolio: myPort,
             myContact: myContact,
             myFooter: myFooter,
             template: './src/index.html',
